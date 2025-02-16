b9365a3871eb3bf3a5c1ae9181056d531c87c159
CertifiedRaveDad <bklingner17@gmail.com>
2025-02-16 14:25:05 -0800

    Update colorChange.js

--- colorChange.js
+++ colorChange.js
@@ -1,4 +1,4 @@
- const colorPicker = document.getElementById("QRCodecolorInput");
+const colorPicker = document.getElementById("QRCodecolorInput");
         const div1 = document.getElementById("svg-container");
         const div2 = document.getElementById("QRcodeText");
 
@@ -6,4 +6,13 @@
             const selectedColor = event.target.value;
             div1.style.color = selectedColor;
             div2.style.color = selectedColor;
+});
+ const bgColorPicker = document.getElementById("QRBackgroundColor");
+        const bg1 = document.getElementById("qrbgcontainer");
+        const bg2 = document.getElementById("QRTextbgcontainer");
+
+        bgColorPicker.addEventListener("input", (event) => {
+            const selectedColor2 = event.target.value;
+            bg1.style.color = selectedColor2;
+            bg2.style.color = selectedColor2;
 });
