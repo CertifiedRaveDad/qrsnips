8a1db5e72949506ee67fac12246842e8c65d7336
CertifiedRaveDad <bklingner17@gmail.com>
2025-02-16 14:36:35 -0800

    Update colorChange.js

--- colorChange.js
+++ colorChange.js
@@ -14,14 +14,13 @@ const bg1 = document.getElementById("qrbgcontainer");
 const bg2 = document.getElementById("QRTextbgcontainer");
 
 function hexToRGBA(hex, alpha = 0.25) {
-    console.log("ran")
     let r = parseInt(hex.substring(1, 3), 16);
     let g = parseInt(hex.substring(3, 5), 16);
     let b = parseInt(hex.substring(5, 7), 16);
     return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 }
 
-bgColorPicker.addEventListener("bgColorPicker", (event) => {
+bgColorPicker.addEventListener("input", (event) => {
     const selectedColor2 = event.target.value;
     const rgbaColor = hexToRGBA(selectedColor2, 0.25)
     bg1.style.backgroundColor = rgbaColor;
