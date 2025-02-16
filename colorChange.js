52d3ea5079fd8b1080f05a48f92d5b3aa4cdf3eb
CertifiedRaveDad <bklingner17@gmail.com>
2025-02-16 14:29:51 -0800

    Update colorChange.js

--- colorChange.js
+++ colorChange.js
@@ -1,18 +1,28 @@
 const colorPicker = document.getElementById("QRCodecolorInput");
-        const div1 = document.getElementById("svg-container");
-        const div2 = document.getElementById("QRcodeText");
+const div1 = document.getElementById("svg-container");
+const div2 = document.getElementById("QRcodeText");
 
-        colorPicker.addEventListener("input", (event) => {
-            const selectedColor = event.target.value;
-            div1.style.color = selectedColor;
-            div2.style.color = selectedColor;
+colorPicker.addEventListener("input", (event) => {
+    const selectedColor = event.target.value;
+    div1.style.color = selectedColor;
+    div2.style.color = selectedColor;
 });
- const bgColorPicker = document.getElementById("QRBackgroundColor");
-        const bg1 = document.getElementById("qrbgcontainer");
-        const bg2 = document.getElementById("QRTextbgcontainer");
 
-        bgColorPicker.addEventListener("input", (event) => {
-            const selectedColor2 = event.target.value;
-            bg1.style.color = selectedColor2;
-            bg2.style.color = selectedColor2;
+
+const bgColorPicker = document.getElementById("QRBackgroundColor");
+const bg1 = document.getElementById("qrbgcontainer");
+const bg2 = document.getElementById("QRTextbgcontainer");
+
+function hexToRGBA(hex, alpha = 0.25) {
+    let r = parseInt(hex.substring(1, 3), 16);
+    let g = parseInt(hex.substring(3, 5), 16);
+    let b = parseInt(hex.substring(5, 7), 16);
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
+
+bgColorPicker.addEventListener("input", (event) => {
+    const selectedColor2 = event.target.value;
+    const rgbaColor = hexToRGBA(selectedColor2, 0.25)
+    bg1.style.color = rgbaColor;
+    bg2.style.color = rgbaColor;
 });
